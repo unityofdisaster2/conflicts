3442dfe8e67f9008b58e1e4ce3f02fffdeabdd87
Raymundo <rrosalesr1500@alumno.ipn.mx>
2021-09-13 22:59:42 -0500

    add factorial func

---
+++ factorial.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int factorial(int number);
+
+int main(int argc, char const *argv[])
+{
+	printf("%d\n", factorial(5));
+	return 0;
+}
+
+int factorial(int number) {
+	if(number <= 2) {
+		return number;
+	} else {
+		return number*factorial(number-1);
+	}
+}
